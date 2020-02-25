63441b2bbc7244158c2b19374b6077cda793e4cd
user9536 <14059614@students.southwales.ac.uk>
2020-02-25 17:51:13 +0000

    minor update

---
+++ Assets/UserInterface/RadialMenu/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Settings : MonoBehaviour
+{
+    public GameObject settings;
+    public Button closeButton;
+    public Button saveButton;
+    bool open;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        settings.SetActive(false);
+        this.GetComponent<Button>().onClick.AddListener(() => { OpenSettings(); });
+        closeButton.onClick.AddListener(() => { CloseSettings(); });
+        saveButton.onClick.AddListener(() => { SaveSettings(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenSettings()
+    {
+        settings.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        settings.SetActive(false);
+    }
+    public void SaveSettings()
+    {
+
+    }
+
+}
--- Assets/UserInterface/RadialMenu/Spell.cs
+++ Assets/UserInterface/RadialMenu/Spell.cs
@@ -2,23 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
 
 public class Spell : MonoBehaviour
 {
-    [HideInInspector]
-    List<Button> childButtons = new List<Button>();
     public GameObject spells;
+    public Button rainButton;
     bool open = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        childButtons = spells.GetComponentsInChildren<Button>(true).
-                        Where(x => x.gameObject.transform.parent != transform.parent).ToList();
         this.GetComponent<Button>().onClick.AddListener(() => { OpenSpells(); });
         spells.SetActive(false);
-
     }
 
     public void OpenSpells()

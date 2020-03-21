4ac42e5eddd7e1800a33cad5385b98de5368427b
user9536 <14059614@students.southwales.ac.uk>
2020-03-21 21:21:00 +0000

    Had to import the basic UI manually because text-mesh pro doesn't work in master branch and it caused issues, will merge from master to UI to get on with inventory work.

---
+++ Assets/UserInterface/Items/Scripts/PlayerGrabController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OVRTouchSample;
+using System;
+using UnityEngine;
+
+public class PlayerGrabController : MonoBehaviour
+{
+    public Interactable focus; //current focus: item or other gameObjects like villagers
+    Camera cam;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if button grabbed
+        //Ray ray = cam.ScreenPointToRay(OVRInput.GetLocalControllerPosition);
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(ray, out hit, 100))
+        //{
+        //  Interactable interactable = hit.collider.GetComponent<Interactable>();
+        //}
+    }
+
+
+}
---
+++ Assets/UserInterface/RadialMenu/Spell.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spell : MonoBehaviour
+{
+    public GameObject spells;
+    public Button rainButton;
+    public Button meteorButton;
+    public GameObject rain;
+    GameObject[] clouds;
+    public GameObject meteor;
+
+    bool open = false;
+    bool raining = false;
+    float rainTime = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(() => { OpenSpells(); });
+        rainButton.GetComponent<Button>().onClick.AddListener(() => { CallRain(); });
+        meteorButton.GetComponent<Button>().onClick.AddListener(() => { CallMeteor(); });
+        spells.SetActive(false);
+    }
+
+    public void OpenSpells()
+    {
+        open = !open;
+        spells.SetActive(open);
+    }
+    public void CallRain()
+    {
+        //StartCoroutine(delay());
+        //WaitForSeconds(rainTime);
+
+        raining = !raining;
+        rain.SetActive(raining);
+    }
+    /*
+    IEnumerator delay()
+    {
+        //FIX TOMORROW
+        yield return new WaitForSeconds(rainTime);
+    }*/
+
+    public void CallMeteor()
+    {
+        Debug.Log("HI");
+    }
+}

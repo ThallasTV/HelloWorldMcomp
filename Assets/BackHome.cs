fa6dda878e69ab793fbed0da6097787ff6dbdb85
Joshua Eatwell <16005481@students.southwales.ac.uk>
2020-04-30 15:05:52 +0100

    Created a portal shader using shader graph, scene transition is done via an event trigger.

---
+++ Assets/BackHome.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackHome : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
